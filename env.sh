b8e5b92c8655e17470e1ee8579fdf2c54fce001b
barry <barryy@dr.com>
2022-07-14 23:37:05 -0700

    updated scripts

--- env.sh
+++ env.sh
@@ -1,10 +1,10 @@
 export TF_VAR_environment_name=devnet_express
 export TF_VAR_kubernetes_cluster_context_name=kind-devnet
 
-### 7. Replace `<access-key>` with your Access Key. Copy and paste from step1 above
+### 7. Replace `<access-key>` with your Access Key. Copy and paste from step1 above. Remove the <>!
 export TF_VAR_panoptica_access_key=<access-key>
 
-### 8. Replace `<secret-key>` with your Access Key. Copy and paste from step1 above
+### 8. Replace `<secret-key>` with your Access Key. Copy and paste from step1 above. Remove the <>!
 export TF_VAR_panoptica_secret_key=<secret-key>
 
 ### 9. Your email for claiming swags!
--- verify-env.sh
+++ verify-env.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-if [[ $TF_VAR_panoptica_access_key == "<access-key>" ]]; then echo "ACCESS KEY Not set!!!"; break; fi
-if [[ $TF_VAR_panoptica_secret_key == "<secret-key>" ]]; then echo "SECRET KEY Not set!!!"; break; fi
-if [[ $VAR_Username == "YOUR-EMAIL-Address" ]]; then echo "Your Email is Not set!!!"; break; else echo "Verification Successful! Please continue..." fi
+regex="^[a-z0-9!#\$%&'*+/=?^_\`{|}~-]+(\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@([a-z0-9]([a-z0-9-]*[a-z0-9])?\.)+[a-z0-9]([a-z0-9-]*[a-z0-9])?\$"
+if [[ -z $TF_VAR_panoptica_access_key ]]||[[ $TF_VAR_panoptica_access_key == "<access-key>" ]]; then echo "ACCESS KEY Not set!!!"; exit 1; fi
+if [[ -z $TF_VAR_panoptica_secret_key ]]||[[ $TF_VAR_panoptica_secret_key == "<secret-key>" ]]; then echo "SECRET KEY Not set!!!"; exit 1; fi
+if [[ $VAR_Username == "YOUR-EMAIL-Address" ]]; then echo "Your Email is Not set!!!"; exit 1; fi
+if [[ $VAR_Username =~ $regex ]]; then echo "Verification Successful! Please continue..."; sh /home/developer/src/dne-appsec/submit.sh 2 $VAR_Username $DEVENV_APP_8080_URL $DEVENV_INSTANCE_ID $DEVENV_USER; else echo "Please provide a valid email address"; fi
