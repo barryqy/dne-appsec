28a89e6b5015e23fe9ed1db80f8a42ec2057fdc9
barry <barryy@dr.com>
2022-08-23 21:55:26 -0700

    Update survey.sh

--- .bash/survey.sh
+++ .bash/survey.sh
@@ -3,4 +3,4 @@ if [[ $VAR_Event != "Yes" ]]; then echo "You had indicated that this is not a pr
 
 echo "Please click on the following link to complete a quick survey"
 echo "https://app.smartsheet.com/b/form/50e33d7cd4a0478b91cde887b043d336?CID="$DEVENV_USER
-submit.sh 5 $VAR_Username $DEVENV_APP_8080_URL $DEVENV_INSTANCE_ID $DEVENV_USER
+/home/developer/src/dne-appsec/.bash/submit.sh 5 $VAR_Username $DEVENV_APP_8080_URL $DEVENV_INSTANCE_ID $DEVENV_USER
