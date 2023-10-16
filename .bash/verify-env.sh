f25f6adf4834a884c9bb6ca517ec13c3e0f776aa
barry <bayuan@cisco.com>
2023-10-15 23:47:50 -0400

    remote webhook

--- .bash/verify-env.sh
+++ .bash/verify-env.sh
@@ -4,4 +4,4 @@ if [[ -z $TF_VAR_panoptica_access_key ]]||[[ $TF_VAR_panoptica_access_key == "<a
 if [[ -z $TF_VAR_panoptica_secret_key ]]||[[ $TF_VAR_panoptica_secret_key == "<secret-key>" ]]; then echo "SECRET KEY Not set!!!"; exit 1; fi
 if [[ $VAR_Username == "YOUR-EMAIL-Address" ]]&&[[ $VAR_Event == "Yes" ]]; then echo "Your Email is Not set!!!"; exit 1; fi
 if [[ $VAR_Event != "Yes" ]]; then echo "Verification done. Please continue..."; fi
-if [[ $VAR_Username =~ $regex ]]&&[[ $VAR_Event == "Yes" ]]; then echo "Verification Successful! Please continue..."; sh /home/developer/src/dne-appsec/.bash/submit.sh 2 $VAR_Username $DEVENV_APP_8080_URL $DEVENV_INSTANCE_ID $DEVENV_USER; else echo "Please provide a valid email address"; fi
+if [[ $VAR_Username =~ $regex ]]&&[[ $VAR_Event == "Yes" ]]; then echo "Verification Successful! Please continue..."; else echo "Please provide a valid email address"; fi
--- env.sh
+++ env.sh
@@ -1,16 +1,15 @@
-echo "export TF_VAR_environment_name=devnet_express" >> /home/developer/.bashrc
-echo "export TF_VAR_kubernetes_cluster_context_name=kind-devnet" >> /home/developer/.bashrc
+export TF_VAR_environment_name=devnet_express
+export TF_VAR_kubernetes_cluster_context_name=kind-devnet
 
 ### 7. Replace `<access-key>` with your Access Key. Copy and paste from step1 above. Remove the <>!
-echo "export TF_VAR_panoptica_access_key=<access-key>" >> /home/developer/.bashrc
+export TF_VAR_panoptica_access_key=<access-key>
 
 ### 8. Replace `<secret-key>` with your Access Key. Copy and paste from step1 above. Remove the <>!
-echo "export TF_VAR_panoptica_secret_key=<secret-key>" >> /home/developer/.bashrc
+export TF_VAR_panoptica_secret_key=<secret-key>
 
 ### 9. Are you attending a proctored event?
-echo "export VAR_Event=Yes" >> /home/developer/.bashrc
+export VAR_Event=No
 
 #### If Yes, provide your email for claiming swags! (with quotes)
-echo "export VAR_Username='YOUR-EMAIL@Address'" >> /home/developer/.bashrc
+export VAR_Username='YOUR-EMAIL@Address'
 
-source /home/developer/.bashrc
