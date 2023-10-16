5948c14dd48524b970bb4a928dff26edef8bd474
barry <bayuan@cisco.com>
2023-10-15 23:52:21 -0400

    Update verify-env.sh

--- .bash/verify-env.sh
+++ .bash/verify-env.sh
@@ -4,4 +4,4 @@ if [[ -z $TF_VAR_panoptica_access_key ]]||[[ $TF_VAR_panoptica_access_key == "<a
 if [[ -z $TF_VAR_panoptica_secret_key ]]||[[ $TF_VAR_panoptica_secret_key == "<secret-key>" ]]; then echo "SECRET KEY Not set!!!"; exit 1; fi
 if [[ $VAR_Username == "YOUR-EMAIL-Address" ]]&&[[ $VAR_Event == "Yes" ]]; then echo "Your Email is Not set!!!"; exit 1; fi
 if [[ $VAR_Event != "Yes" ]]; then echo "Verification done. Please continue..."; fi
-if [[ $VAR_Username =~ $regex ]]&&[[ $VAR_Event == "Yes" ]]; then echo "Verification Successful! Please continue..."; else echo "Please provide a valid email address"; fi
+if [[ $VAR_Username =~ $regex ]]&&[[ $VAR_Event == "Yes" ]]; then echo "Verification Successful! Please continue..."; else echo "Note: If you're attending Panoptica Workshop, please provide a valid email address"; fi
