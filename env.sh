3bc4606378401e60caa1a3f5338d17c1fb699164
barry <bayuan@cisco.com>
2024-02-07 15:12:47 +0100

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,15 +1,8 @@
 export TF_VAR_environment_name=devnet_express
 export TF_VAR_kubernetes_cluster_context_name=kind-devnet
 
-### 7. Replace `<access-key>` with your Access Key. Copy and paste from step1 above. Remove the <>!
-export TF_VAR_panoptica_access_key=<access-key>
+### 1. Replace `access-key` with your Access Key. Copy and paste from last step
+export TF_VAR_panoptica_access_key="access-key"
 
-### 8. Replace `<secret-key>` with your Access Key. Copy and paste from step1 above. Remove the <>!
-export TF_VAR_panoptica_secret_key=<secret-key>
-
-### 9. Are you attending a proctored event?
-export VAR_Event=No
-
-#### If Yes, provide your email for claiming swags! (with quotes)
-export VAR_Username='YOUR-EMAIL@Address'
-
+### 2. Replace `secret-key` with your Access Key. Copy and paste from last step
+export TF_VAR_panoptica_secret_key="secret-key"
